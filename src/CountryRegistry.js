8ff839cce5d6e43d5e6a0acf3c8d683e7fefe62e
MilenaSavatovic <96110742+MilenaSavatovic@users.noreply.github.com>
2022-02-12 14:50:04 +0100

    added axios

--- src/CountryRegistry.js
+++ src/CountryRegistry.js
@@ -1,12 +1,41 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 
 export default function CountrRegistry() {
+  let [keyword, steKeyword] = useState(null)
+
+  function search() {
+    let url = `https://restcountries.com/v3.1/name/${keyword}`
+
+    axios.get(url).then(handleResponse)
+  }
+
+  function handleResponse(response) {
+    console.log(response)
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    search()
+  }
+
+  function handleWordChange(event) {
+    event.preventDefault()
+    steKeyword(event.target.value)
+  }
+
   return (
     <div className="CountryRegistry">
-      <form>
-        <input type="search" placeholder="Type country" />
-        <input type="submit" value="search" className="btn btn-primary" />
-      </form>
+      <section>
+        <form className="form-group" onSubmit={handleSubmit}>
+          <input
+            type="search"
+            placeholder="Type country"
+            onChange={handleWordChange}
+          />
+          <input type="submit" value="search" className="btn btn-primary" />
+        </form>
+      </section>
     </div>
   )
 }
